032e652f5d818a0f5dc0f6665677fdcea438d668
Rohit <rohitjha986@gmail.com>
2020-05-31 14:50:06 +0530

    styling heading section as per standard

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Heading extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="container heading"> 
-            <p className="heading-message">Where in the world?</p>
-            
-            <p  className="heading-theme"> <i className="fa fa-moon-o" style={{fontSize:14}}></i> Dark Mode</p>
-            </div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+        <div className="container heading">
+          <p className="heading-message">Where in the world?</p>
+
+          <p className="heading-theme">
+            {" "}
+            <i className="fa fa-moon-o" style={{ fontSize: 14 }}></i> Dark Mode
+          </p>
+        </div>
+    );
+  }
 }
- 
+
 export default Heading;
