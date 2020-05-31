c16757b135f215f907de49586f03d1963e32a018
Rohit <rohitjha986@gmail.com>
2020-05-31 23:25:17 +0530

    details page css added for mobile

--- src/components/CountryDetailsComponent.jsx
+++ src/components/CountryDetailsComponent.jsx
@@ -28,7 +28,7 @@ class Country extends Component {
               <div className="country-detailsPage-detailsTitle">Population:<p style={{display:'inline'}}>{this.props.country.population}</p></div>
               <div className="country-detailsPage-detailsTitle">Region:<p style={{display:'inline'}}>{this.props.country.region}</p></div>
               <div className="country-detailsPage-detailsTitle">Sub Region:<p style={{display:'inline'}}>{this.props.country.subregion}</p></div>
-              <div className="country-detailsPage-detailsTitle"> Capital:<p style={{display:'inline'}}>{this.props.country.capital}</p></div>
+              <div className="country-detailsPage-detailsTitle capital"> Capital:<p style={{display:'inline'}}>{this.props.country.capital}</p></div>
               <div className="country-detailsPage-detailsTitle">Top Level Domain:<p style={{display:'inline'}}>{this.props.country.topLevelDomain[0]}</p></div>
               <div className="country-detailsPage-detailsTitle">Currencies:<p style={{display:'inline'}}>{this.props.country.currencies[0].name}</p></div>
               <div className="country-detailsPage-Languages"><div className="country-detailsPage-detailsTitle">
