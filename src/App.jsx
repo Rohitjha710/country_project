76720b8827b51120f9a6ee1fd2b32dd71b38a079
Rohit <rohitjha986@gmail.com>
2020-06-01 14:21:35 +0530

    dark theme set fro homepage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
     render() { 
         return (
         <Router>
-            <ThemeProvider theme={{mode:'dark'}}>
+            <ThemeProvider theme={{mode:'light'}}>
             <GlobalStyle/>
             <Route exact path="/">
             
--- src/Themes.js
+++ src/Themes.js
@@ -20,8 +20,34 @@ const GlobalStyle = createGlobalStyle`
  box-shadow:${props=>props.theme.mode==='light'?'':'none'};
  border: ${props=>props.theme.mode==='light'?'':'1px solid hsl(209,23%,25%)'};
 }
-input{
+input,select{
     color:${props=>props.theme.mode==='light'?'':'white'};
+}
+.region-options{
+    background-color:${props=>props.theme.mode==='light'?'':'hsl(209, 23%, 22%)'};
+    box-shadow:${props=>props.theme.mode==='light'?'':'none'};
+    border:${props=>props.theme.mode==='light'?'':'none'};
+}
+.countries{
+    background-color:${props=>props.theme.mode==='light'?'':'hsl(207, 26%, 17%)'};
+}
+.country-homepage{
+    background-color:${props=>props.theme.mode==='light'?'':'hsl(209, 23%, 22%)'};
+    color:${props=>props.theme.mode==='light'?'':'hsl(0, 0%, 100%)'};
+    box-shadow:${props=>props.theme.mode==='light'?'':'4px 4px hsl(209, 23%, 24%);'};
+}
+.country-details-homepage{
+    color:${props=>props.theme.mode==='light'?'':'hsl(0, 0%, 100%)'};
+   
+}
+.country-details-homepage p{
+    color:${props=>props.theme.mode==='light'?'':'hsl(0, 0%, 70%)'};
+   
+}
+.country-details-main-section,html{
+    background-color:${props=>props.theme.mode==='light'?'':'hsl(207, 26%, 17%)'};
+    border-top: ${props=>props.theme.mode==='light'?'':'4px outset hsl(207, 26%, 18%)'};
+
 }
 `;
 export default GlobalStyle;
