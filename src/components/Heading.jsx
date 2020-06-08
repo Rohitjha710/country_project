0159c35698cacf05d5f68ca8bf04649c4cd2e453
Rohit <rohitjha986@gmail.com>
2020-06-08 11:31:25 +0530

    using funtional component for heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { Flex,Text} from "@chakra-ui/core";
-class Heading extends Component {
-  state = {};
-  render() {
+function Heading(props) {
+
     return (
       <Flex justify="space-between" px="5.3%" bg=" hsl(0, 0%, 99.5%)">
           <Text  fontWeight="800" fontSize="1.2rem">Where in the world?</Text>
 
-          <Text fontWeight="600" fontSize="0.8rem" pt="0.8rem" onClick={this.props.changeTheme}>
+          <Text fontWeight="600" fontSize="0.8rem" pt="0.8rem" onClick={props.changeTheme}>
             {" "}
             <i className="fa fa-moon-o" style={{ fontSize: 14 }}></i> Dark Mode
           </Text>
         </Flex>
     );
-  }
+  
 }
 
 export default Heading;
